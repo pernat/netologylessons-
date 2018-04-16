8d1535ca5557efdcc47b44b990e8ac991f1e1c77
szhuravlev <at-pernat@ya.ru>
2018-04-16 22:54:39 +0300

    Try

--- Lec2.1/main.py
+++ Lec2.1/main.py
@@ -1,24 +1,43 @@
-cook_book = {
-  'яйчница': [
-    {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-    ],
-  'стейк': [
-    {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-    {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
-    {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
-    ],
-  'салат': [
-    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-    {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
-    {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-    {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-    ]
-  }
+with open('dict.txt') as file:
+    menu_file = file.read()
+    menu_file = menu_file.splitlines()
+    file.close()
+
+
+def get_cook_book():
+    diction = dict()
+    dic2 = dict()
+    temp = list()
+    z1 = 0
+    z2 = 1
+    for z, y in enumerate(menu_file):
+        if y in '':
+            temp = []
+            z1 = z+1
+            z2 = z+2
+            continue
+        if z == z1:
+            diction[y.lower()] = temp
+            continue
+        elif z == z2:
+            dic2['persons'] = y
+            temp.append(dic2)
+            dic2 = dict()
+            continue
+        spl = y.split(' | ')
+        dic2['ingridient_name'] = spl[0]
+        dic2['quantity'] = int(spl[1])
+        dic2['measure'] = spl[2]
+
+        temp.append(dic2)
+        dic2 = dict()
+    return diction
+
 
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
+    cook_book = get_cook_book()
     for dish in dishes:
         for ingridient in cook_book[dish]:
             new_shop_list_item = dict(ingridient)
