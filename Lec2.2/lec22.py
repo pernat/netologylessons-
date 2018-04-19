52815e12ca12f4cdb1986e6edbc669a6fc9813c6
szhuravlev <at-pernat@ya.ru>
2018-04-19 21:48:54 +0300

    Домашняя работа к лекции 2.2 «Работа с кодировками, русскими буквами»

--- Lec2.2/lec22.py
+++ Lec2.2/lec22.py
@@ -2,43 +2,39 @@ import os
 import chardet
 from collections import Counter
 
-# Указываем переменные содержащие путь
-f_list_adr = os.path.dirname(__file__)
-f_list = os.listdir(path=f_list_adr + '/PY1_Lesson_2.3/')
-
 file_encoding = dict()
-test = list()
 
 
-# Создаем словарь с кодировками для кажддого файла
-for file_name in f_list:
-    with open(f_list_adr + '/PY1_Lesson_2.3/' + file_name, 'rb') as f:
-        data = f.read()
-        result = chardet.detect(data)
-        print(result)
-        file_encoding[file_name] = result['encoding']
+def get_path():
+    f_list_adr = os.path.dirname(__file__)
+    return f_list_adr
 
 
-# Открываем файлы
-for name in f_list:
-    print(name)
-    with open(f_list_adr + '/PY1_Lesson_2.3/' + name, 'r', encoding=file_encoding[name]) as f:
-        data = f.read()
-        data = data.split(' ')
-        data.sort()
-        for i in data:
-            # print(i.split())
-            if len(i) > 6:
-                test.append(i)
-        count = Counter(test)
-        key_word = max(count, key=count.get)
-        print(key_word)
-        print(test)
-        for item in test:
-            if item in key_word:
-                test.remove(key_word)
-        print('SDFSFSDF', test)
+def get_file_list():
+    f_list = os.listdir(path=get_path() + '/PY1_Lesson_2.3/')
+    return f_list
 
-        # print(data)
-        # print(count)
 
+def get_job():
+    for file_name in get_file_list():
+        with open(get_path() + '/PY1_Lesson_2.3/' + file_name, 'rb') as f:
+            data1 = f.read()
+            result = chardet.detect(data1)
+            file_encoding[file_name] = result['encoding']
+    for name in get_file_list():
+        print('Наиболее часто встречающиеся слова в файле {}:'.format(name))
+        with open(get_path() + '/PY1_Lesson_2.3/' + name, 'r', encoding=file_encoding[name]) as f:
+            data = f.read()
+            data = data.split(' ')
+            data.sort()
+            word_list = list()
+            for i in data:
+                if len(i) > 6:
+                    word_list.append(i.lower())
+            count = Counter(word_list)
+            print(count.most_common(10), '\n')
+
+
+get_path()
+get_file_list()
+get_job()
