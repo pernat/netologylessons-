e76b19ae039d24ed1f96fcdf08750039b4122fc5
Сергей Журавлев <at-pernat@ya.ru>
2018-04-23 23:48:50 +0300

    test file

--- Lec2.4/test.py
+++ Lec2.4/test.py
@@ -1,14 +1,5 @@
 import os.path
-# import chardet
 from chardet.universaldetector import UniversalDetector
-
-
-# migrations = 'Migrations'
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_encoding = dict()
-detector = UniversalDetector()
-
-q = input('Введите ключевые слова: ')
 
 
 def get_path():
@@ -22,8 +13,12 @@ def get_file_list():
 
 
 def get_job():
-    # Это была неудачная попытка определять кодировку sql файлов
-    for file_name in get_file_list():
+    file_list = get_file_list()
+    yal = list()
+    file_encoding = dict()
+    detector = UniversalDetector()
+    # Это была неудачная попытка определять кодировку sql файлов, но почему то без этого не работает
+    for file_name in file_list:
         with open(get_path() + '/Migrations/' + file_name, 'rb') as f:
             for line in f:
                 detector.feed(line)
@@ -32,14 +27,23 @@ def get_job():
                     file_encoding[file_name] = result['encoding']
                     break
                 detector.close()
-    for name in get_file_list():
-        filename, file_extension = os.path.splitext(name)
-        if '.sql' in file_extension:
-            with open(get_path() + '/Migrations/' + name, 'r') as fdd:
-                data = fdd.read()
-                data = data.split()
-                # Получаем список файлов и количества слов в них
-                print('В файле', name, 'совпаденией - \n', data.count(q))
+    new_file_list = file_list
+
+    while True:
+        q = input('Введите ключевые слова для поиска или "exit" для выхода: ')
+        if q == 'exit':
+            break
+        for name in new_file_list:
+            filename, file_extension = os.path.splitext(name)
+            if '.sql' in file_extension:
+                with open(get_path() + '/Migrations/' + name, 'r') as fdd:
+                    data = fdd.read()
+                    data = data.split()
+                if data.count(q) > 0:
+                    print('В файле', name, 'совпаденией: \n', data.count(q))
+                else:
+                    new_file_list.remove(name)
+        print(len(new_file_list))
 
 
 get_path()
