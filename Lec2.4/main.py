dcb5d3d90ca5e32d153ca78512fff3889e79e318
Sergey <at-pernat@ya.ru>
2018-04-24 23:45:55 +0300

    Готовый вариант

--- Lec2.4/main.py
+++ Lec2.4/main.py
@@ -1,13 +1,7 @@
-import os
-import chardet
-from collections import Counter
-
-
-migrations = 'Migrations'
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_encoding = dict()
-
-q = input('Введите ключевые слова: ')
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os.path
+from chardet.universaldetector import UniversalDetector
 
 
 def get_path():
@@ -16,29 +10,49 @@ def get_path():
 
 
 def get_file_list():
-    f_list = os.listdir(path=get_path() + '/PY1_Lesson_2.3/')
+    f_list = os.listdir(path=get_path() + '/Migrations/')
     return f_list
 
 
 def get_job():
-    for file_name in get_file_list():
-        with open(get_path() + '/PY1_Lesson_2.3/' + file_name, 'rb') as f:
-            data1 = f.read()
-            result = chardet.detect(data1)
-            file_encoding[file_name] = result['encoding']
-    for name in get_file_list():
-        print('Наиболее часто встречающиеся слова в файле {}:'.format(name))
-        with open(get_path() + '/PY1_Lesson_2.3/' + name, 'r', encoding=file_encoding[name]) as f:
-            data = f.read()
-            data = data.split(' ')
-            data.sort()
-            word_list = list()
-            for i in data:
-                if len(i) > 6:
-                    word_list.append(i.lower())
-            count = Counter(word_list)
-            print(count.most_common(10), '\n')
+    file_list = get_file_list()
+    file_encoding = dict()
+    detector = UniversalDetector()
+    # Это была неудачная попытка определять кодировку sql файлов, но почему то без этого не работает
+    for file_name in file_list:
+        with open(get_path() + '/Migrations/' + file_name, 'rb') as f:
+            for line in f:
+                detector.feed(line)
+                if detector.done:
+                    result = detector.result
+                    file_encoding[file_name] = result['encoding']
+                    break
+                detector.close()
+    new_file_list = file_list
+    new_file_list2 = list()
+    x = 0
+    while True:
+        q = input('Введите ключевые слова для поиска или "exit" для выхода: ')
+        if q == 'exit':
+            break
+        if x > 0:
+            new_file_list = new_file_list2
+            new_file_list2 = list()
+        if len(new_file_list) == 0:
+            print('К сожалению больше ни чего не найдено')
+            exit()
+        for name in new_file_list:
+            filename, file_extension = os.path.splitext(name)
+            if '.sql' in file_extension:
+                with open(get_path() + '/Migrations/' + name, 'r') as fdd:
+                    data = fdd.read()
+                    data = data.split()
+                if data.count(q) > 0:
+                    print('В файле', name, 'совпаденией: \n', data.count(q))
+                    new_file_list2.append(name)
+                else:
+                    continue
+        x += 1
 
-get_path()
-get_file_list()
+
 get_job()
