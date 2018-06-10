0c67e6c11aa1a3c7ba705db8ecaeaba423c040e5
Сергей Журавлев <at-pernat@ya.ru>
2018-06-10 22:15:21 +0300

    Диплом

--- Diplom/main.py
+++ Diplom/main.py
@@ -20,6 +20,10 @@ def get_user_friends():
     return friends['response']['items']
 
 
+def group(iterable, count):
+    return zip(*[iter(iterable)] * count)
+
+
 def get_user_groups():
     groups_list = 'https://api.vk.com/method/groups.get'
     request = requests.get(groups_list,
@@ -34,22 +38,29 @@ def get_user_groups():
     return groups
 
 
-def get_is_member():
+def get_is_member(a):
     answer_list = list()
+    users = a[0]
+    str_users = ", ".join(str(x) for x in users)
     for group in get_user_groups():
+        print(group)
         groups_list = 'https://api.vk.com/method/groups.isMember'
         request = requests.get(groups_list,
                                params=dict(
                                    group_id=group,
                                    access_token=TOKEN,
-                                   user_id='624576',
+                                   user_ids=str_users,
                                    v='5.74'
                                )
                                )
         groups = request.json()
-        answer_list.append([group, groups])
-        time.sleep(1)
+        answer_list.append([group, groups['response']])
+        time.sleep(0.5)
     return answer_list
 
 
-pprint(get_is_member())
+split = lambda x, n: x if not x else [x[:n]]+[split([] if not -(len(x)-n) else x[-(len(x)-n):], n)][0]
+
+a = split(get_user_friends(), 300)
+
+pprint(get_is_member(a))
